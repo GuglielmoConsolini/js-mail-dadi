1523b2a0970b0ccfa7d537e82a8bfb834eb08521
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-04-22 18:24:54 +0200

    JS dadi + email completi con break

--- js/dadi.js
+++ js/dadi.js
@@ -1,22 +1,37 @@
+const myContainer = document.querySelector('div');
+const pulsante = document.getElementById("game")
+
+pulsante.addEventListener("click" , function(){
 
 let numeroGiocatore = Math.floor(Math.random() * 6 + 1);
-
 console.log(numeroGiocatore);
 
 let numeroComputer= Math.floor(Math.random() * 6 + 1);
-
 console.log(numeroComputer);
 
 if (numeroGiocatore > numeroComputer){
 
     console.log("Ha vinto il giocatore")
+    const result = document.createElement("div")
+    result.append("Ha vinto il giocatore")
+    myContainer.append(result)
 
 } else if (numeroGiocatore < numeroComputer){
 
     console.log("Ha vinto il Computer")
+    const result = document.createElement("div")
+    result.append("Ha vinto il computer")
+    myContainer.append(result)
 
 } else {
 
      console.log("Pareggio")
+     const result = document.createElement("div")
+    result.append("Pareggio")
+    myContainer.append(result)
 
 }
+
+
+})
+
--- js/email.js
+++ js/email.js
@@ -2,7 +2,7 @@
 const emailAccesso = [
 
 "pippo@gmail.com",
-"margo@gmail.com",
+"marco@gmail.com",
 "guglielmo@gmail.com",
 "nikolas@gmail.com",
 "giuseppe@gmail.com",
@@ -16,24 +16,21 @@ let eMail = prompt("Inserisci Email..");
 
 for (let i = 0; i < emailAccesso.length; i++) {
 
-    
-
 	const dirittoDiaccesso = emailAccesso[i];
-
     console.log(dirittoDiaccesso)
-
 
     if (eMail == dirittoDiaccesso){
 
     console.log("Hai accesso")
     document.getElementById("esito").innerText = "Hai accesso"
+    break
     
 
     } else {
 
      console.log("non hai accesso")
      document.getElementById("esito").innerText = "Non hai accesso"
-
+     break
     }
 
 	
